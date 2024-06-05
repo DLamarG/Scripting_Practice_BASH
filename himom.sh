c4b07f66ab35a829fd563f01ddc3b2aab1c281f3
DLamarG <demondg8@gmail.com>
2024-06-04 22:59:01 -0500

    two scripts complete

---
+++ himom.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+echo "Hello Mom" 
+
+sleep 3
+
+echo "umm sure"
+
+sleep 3
+
+echo "that sounds like fun"
+
+sleep 3
+
+echo "Sounds good"
+
+sleep 3 
+
+echo "Talk to you later, love you"
